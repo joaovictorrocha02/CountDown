3d4c39ccd6a25fdff5062f7699db3a62b5cdcf55
Aluno <Aluno@LAB04-PC10>
2024-07-02 20:44:58 -0300

    Commit project CountDown

---
+++ src/Countdown.java
@@ -0,0 +1,15 @@
+public class Countdown {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Countdown to Launch: ");
+
+        for(int i = 20; i >= 0; i--) {
+            System.out.print(i +" "); 
+        }
+
+        System.out.println("Blast Off!");
+        
+        //Irá contar em ordem descrescente do 20 ao 0
+    }
+}
